ffcd93590f2e0643accd63b1338589270caf6e4f
Rakib <rh.rakibhasan365@gmail.com>
2025-05-09 23:47:33 +0600

    assignment-1

---
+++ problem.ts
@@ -0,0 +1,60 @@
+{
+    //
+    // Problem1
+    function formatString(input: string, toUpper?: boolean): string  {
+        return toUpper === true ? input.toUpperCase() : input.toLowerCase();
+    }
+    const result = "Hello";
+    formatString(result);
+
+    // Problem2
+    function filterByRating(books: { title: string; rating: number }[]) {
+        return books.filter(book => book.rating > 4.0);
+      }
+
+      const books = [
+        { title: "Book A", rating: 4.5 },
+        { title: "Book B", rating: 3.2 },
+        { title: "Book C", rating: 5.0 }
+      ];
+      
+    filterByRating(books); 
+      
+    // Problem4
+    class Vehicle {
+        private make: string;
+        private year: number;
+      
+        constructor(make: string, year: number) {
+          this.make = make;
+          this.year = year;
+        }
+      
+        public getInfo(): string {
+          return `Make: ${this.make}, Year: ${this.year}`;
+        }
+      }
+      
+      class Car extends Vehicle {
+        private model: string;
+      
+        constructor(make: string, year: number, model: string) {
+          super(make, year);
+          this.model = model;
+        }
+      
+        public getModel(): string {
+          return `Model: ${this.model}`;
+        }
+      }
+      
+      const myCar = new Car("Toyota", 2020, "Corolla");
+      console.log(myCar.getInfo());
+      console.log(myCar.getModel());
+      
+    // Problem5
+    
+    
+    
+    //
+}
